6c1034be32475f29c047f1c5e3c2c32ee73e4b36
Dhanasekaran22 <dhanasekaran2622@gmail.com>
2024-12-18 10:20:15 +0530

    coding challenges updated

--- N_DoubleExists.js
+++ N_DoubleExists.js
@@ -4,7 +4,7 @@ function doubleExists(elements) {
     var count = 0;
     for (var _i = 0, inputNumber_1 = inputNumber; _i < inputNumber_1.length; _i++) {
         var element = inputNumber_1[_i];
-        if (inputNumber.indexOf(0, 1) != inputNumber.indexOf(element)) {
+        if (element == 0 && inputNumber.indexOf(0) !== inputNumber.lastIndexOf(0)) { // correct the logic 
             return true;
         }
         else if (inputNumber.includes(element + element) && element != 0) {
@@ -13,6 +13,6 @@ function doubleExists(elements) {
     }
     return false;
 }
-console.log(doubleExists([2, 5, 3, 0]));
+console.log(doubleExists([2, 5, 3, 0])); // output is false but here it shows true correct the code
 console.log(doubleExists([10, 2, 5, 3, 0]));
 console.log(doubleExists([3, 1, 7, 11, 0, 0]));
--- N_DoubleExists.ts
+++ N_DoubleExists.ts
@@ -3,7 +3,7 @@ function doubleExists(elements:number[]):boolean{
     console.log(inputNumber);
     let count=0;
     for(let element of inputNumber){
-        if(inputNumber.indexOf(0,1)!=inputNumber.indexOf(element)){     // correct the logic 
+        if(element==0 && inputNumber.indexOf(0) !== inputNumber.lastIndexOf(0)){     
             return true;
             
         }
@@ -14,6 +14,6 @@ function doubleExists(elements:number[]):boolean{
     }
     return false;
 }
-console.log(doubleExists([2,5,3,0]))        // output is false but here it shows true correct the code
+console.log(doubleExists([2,5,3,0]))        
 console.log(doubleExists([10,2,5,3,0]))
 console.log(doubleExists([3,1,7,11,0,0]));
