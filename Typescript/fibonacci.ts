2373cd1c62233b236269ee4fd90dc575ad123bae
Dhanasekaran22 <dhanasekaran2622@gmail.com>
2025-01-07 14:15:04 +0530

    coding challenges updated

--- Typescript/fibonacci.ts
+++ Typescript/fibonacci.ts
@@ -5,7 +5,7 @@ var num: number = parseInt(getInput);
 let sumOfElement: number[] = [0, 1];
 for (let i = 2; i < num; i++) {
 
-    sumOfElement.push(sumOfElement[i-2]+sumOfElement[i-1]);
+    sumOfElement.push(sumOfElement[i-2]+sumOfElement[i-1])
 
 }
 
